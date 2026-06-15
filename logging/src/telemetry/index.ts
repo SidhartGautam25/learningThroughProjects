69a8425ccffd3370a07378b6bb14d297ad2fdda1
Sidharth Gautam <navneetnishchal1420@gmail.com>
2026-06-15 08:32:34 +0530

    trying to run the application

--- logging/src/telemetry/index.ts
+++ logging/src/telemetry/index.ts
@@ -1,28 +1,68 @@
+// import { NodeSDK } from "@opentelemetry/sdk-node";
+// import { ConsoleSpanExporter, SimpleSpanProcessor} from "@opentelemetry/sdk-trace-base";
+// import { getNodeAutoInstrumentations } from "@opentelemetry/auto-instrumentations-node";
+
+// const sdk = new NodeSDK({
+//   spanProcessors: [
+//     new SimpleSpanProcessor(new ConsoleSpanExporter()),
+//   ],
+//   instrumentations: [getNodeAutoInstrumentations()],
+// });
+
+// export async function startTelemetry() {
+//   await sdk.start();
+// }
+
+
+// /*
+
+// ConsoleSpanExporter - prints the traces to console in json format
+// SimpleSpanProcessor - sends the spans to the exporter ( bad for production)
+// BatchSpanProcessor - sends the spans to the exporter in batches ( good for production)
+
+
+
+
+
+
+//  */
+
+// above one is the code of phase 1
+
 import { NodeSDK } from "@opentelemetry/sdk-node";
-import { ConsoleSpanExporter, SimpleSpanProcessor} from "@opentelemetry/sdk-trace-base";
-import { getNodeAutoInstrumentations } from "@opentelemetry/auto-instrumentations-node";
+
+import {
+  BatchSpanProcessor,
+} from "@opentelemetry/sdk-trace-base";
+
+import {
+  OTLPTraceExporter,
+} from "@opentelemetry/exporter-trace-otlp-http";
+
+import {
+  getNodeAutoInstrumentations,
+} from "@opentelemetry/auto-instrumentations-node";
+
+const traceExporter =
+  new OTLPTraceExporter({
+    url:
+      "http://localhost:4318/v1/traces",
+  });
 
 const sdk = new NodeSDK({
+
   spanProcessors: [
-    new SimpleSpanProcessor(new ConsoleSpanExporter()),
+    new BatchSpanProcessor(
+      traceExporter
+    ),
   ],
-  instrumentations: [getNodeAutoInstrumentations()],
+
+  instrumentations: [
+    getNodeAutoInstrumentations(),
+  ],
+
 });
 
 export async function startTelemetry() {
   await sdk.start();
 }
-
-
-/*
-
-ConsoleSpanExporter - prints the traces to console in json format
-SimpleSpanProcessor - sends the spans to the exporter ( bad for production)
-BatchSpanProcessor - sends the spans to the exporter in batches ( good for production)
-
-
-
-
-
-
- */
